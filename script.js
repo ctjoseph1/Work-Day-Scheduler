2c37c03800bbb16f4e83943b2e29c7245b48ed44
ctjoseph1 <79760037+ctjoseph1@users.noreply.github.com>
2023-12-11 19:29:59 +0000

    linked Js file to html and pasted in random code into js to test

---
+++ script.js
@@ -0,0 +1,9 @@
+var date = new date ();
+alert (date) ;
+var defaultDate = $( ".selector" ).datepicker( "option", "defaultDate" );
+$.datepicker.setDefaults({
+    showOn: "both",
+    buttonImageOnly: true,
+    buttonImage: "calendar.gif",
+    buttonText: "Calendar"
+  });
