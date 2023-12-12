3e9c50121b31db862f81b230503d9b98a56d0df0
ctjoseph1 <79760037+ctjoseph1@users.noreply.github.com>
2023-12-12 18:25:09 +0000

    added pseudo notes and current day/ savBtn test

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-var date = new date ();
-alert (date) ;
-var defaultDate = $( ".selector" ).datepicker( "option", "defaultDate" );
-$.datepicker.setDefaults({
-    showOn: "both",
-    buttonImageOnly: true,
-    buttonImage: "calendar.gif",
-    buttonText: "Calendar"
-  });
+
+
+  $(".saveBtn").on("click",function(){
+    console.log("test", this)
+  })
+  console.log("js loaded", dayjs().format("DD/MM/YY"));
+$("#currentDay").text(dayjs().format("DD/M/YY")) //How to have month spelled out in text?
+
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+  // >> Present time is Red, Past time is Grey, future time is Green
+
+// Allow a user to enter an event when they click a timeblock
+  // >>Done using text area html element
+
+// Save the event in local storage when the save button is clicked in that timeblock.
+    // >>Persist events between refreshes of a page
+    //>>Wipes out data when it's a new day- Best approach is an array
+
